7c1c7e78a75391689ee3d3ecac8ed1fccafac7aa
Richardxcn5 <richardxcn5@gmail.com>
2021-05-18 23:37:50 -0700

    rewrote entire code from scratch

--- app/src/main/java/com/example/linegame/MainActivity.java
+++ app/src/main/java/com/example/linegame/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.linegame;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.*;
 
 import android.os.Bundle;
 
@@ -11,4 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void startGame(View v){
+        System.out.println("ye boi");
+    }
+
 }
